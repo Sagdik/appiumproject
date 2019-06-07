4fc5aff29b5df0931a049cfd2872c9b9731ec413
Sagar Tiwari <sanutiwari7@gmail.com>
2019-06-08 01:31:01 +0800

    New appium project change

---
+++ appium.io/src/main/java/appium/io/base/BaseTest.java
@@ -0,0 +1,21 @@
+package appium.io.base;
+
+import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class BaseTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		DesiredCapabilities capabilities= new DesiredCapabilities();
+		
+		//set the capability
+		
+		
+		
+		
+		
+	}
+
+}
